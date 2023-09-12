2b79b0622b9ecab0f3f1d2c6030e375e6e6a1b60
Chris-Effah <akboateng41@gmail.com>
2023-09-12 13:27:00 -0700

    beautiful code

---
+++ chrisliz.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def factorial(x):
+    """calculating the factorial of a non-negative integer"""
+    if x < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    if x == 0:
+        return 1
+    result = 1
+
+    for i in range(1, x + 1):
+        result *= i
+    return result
+
+
+def main():
+    """main function to get user input and calculate factorial"""
+    try:
+        num = int(input("Enter a non-negative integer:"))
+        result = factorial(num)
+        print(f"The factorial of {num} is {result}")
+    except ValueError as e:
+        print(f"Error: {e}")
+    except KeyboardInterrupt:
+        print("\nOperation aborted by user.")
+
+
+if __name__ == "__main__":
+    main()
