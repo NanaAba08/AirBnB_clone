89d1ed222b994770ac7a1559e6dbb399d340bc00
Chris-Effah <akboateng41@gmail.com>
2023-09-14 11:53:23 -0700

    console

--- console.py
+++ console.py
@@ -70,96 +70,96 @@ class HBNBCommand(cmd.Cmd):
 
         all_instances = storage.all()
         key = "{}.{}".format(class_name, instance_id)
-        instance = all_instance.get(key, None)
+        instance = all_instances.get(key, None)
         if instance:
             print(instance)
         else:
             print("** no instance found **")
 
-        def do_destroy(self, args):
-            if not args:
-                print("** class name missing **")
-                return
+    def do_destroy(self, args):
+        if not args:
+            print("** class name missing **")
+            return
 
-            arg_list = args.split()
-            if len(args_list) < 2:
-                print("** instance id missing **")
-                return
+        arg_list = args.split()
+        if len(args_list) < 2:
+            print("** instance id missing **")
+            return
 
-            class_name = args_list[0]
-            instance_id = args_list[1]
+        class_name = args_list[0]
+        instance_id = args_list[1]
 
-            if not self.check_class(class_name):
-                print("** class doesn't exist **")
-                return
+        if not self.check_class(class_name):
+            print("** class doesn't exist **")
+            return
 
-            all_instances = storage.all()
-            key = "{}.{}".format(class_name, instance_id)
-            instance = all_instance.get(key, None)
-            if instance:
-                del all_intances[key]
-                storage.save()
-            else:
-                print("** no instance found **")
+        all_instances = storage.all()
+        key = "{}.{}".format(class_name, instance_id)
+        instance = all_instances.get(key, None)
+        if instance:
+            del all_intances[key]
+            storage.save()
+        else:
+            print("** no instance found **")
         
-        def do_all(self, args):
-            class_name = args.split()[0] if args else None
+    def do_all(self, args):
+        class_name = args.split()[0] if args else None
 
 
-            if class_name and not self.check_class(class_name):
-                print("** class doesn't exist **")
-                return
+        if class_name and not self.check_class(class_name):
+            print("** class doesn't exist **")
+            return
 
-            all_instances = storage.all()
-            instances = []
-            for key, instance in all_instances.items():
-                if class_name is None or key.stratwith(class_name):
-                    instances.append(str(instance))
-                print(instances)
+        all_instances = storage.all()
+        instances = []
+        for key, instance in all_instances.items():
+            if class_name is None or key.startswith(class_name):
+                instances.append(str(instance))
+        print(instances)
 
-        def do_update(self, args):
-            args_list = args.split()
+    def do_update(self, args):
+        args_list = args.split()
 
-            if not args:
-                print("** class name missing **")
-                return
+        if not args:
+            print("** class name missing **")
+            return
 
-            if len(args_list) < 2:
-                print("** instance id missing **")
-                return
+        if len(args_list) < 2:
+            print("** instance id missing **")
+            return
             
-            if len(args_list) < 3:
-                print("** attribute name missing **")
-                return
+        if len(args_list) < 3:
+            print("** attribute name missing **")
+            return
 
-            if len(args_list) < 4:
-                print("** value missing **")
-                return
+        if len(args_list) < 4:
+            print("** value missing **")
+            return
 
-            class_name = args_list[0]
-            instance_id = args_list[1]
-            attribute_name = args_list[2]
-            attribute_value = args_list[3]
+        class_name = args_list[0]
+        instance_id = args_list[1]
+        attribute_name = args_list[2]
+        attribute_value = args_list[3]
 
-            if not self.check_class(class_name):
-                print("** classs doesn't exist **")
-                return
+        if not self.check_class(class_name):
+            print("** classs doesn't exist **")
+            return
 
-            all_instances = storage.all()
-            key = "{}.{}".format(class_name, instance_id)
-            instance = all_instances.get(key, None)
+        all_instances = storage.all()
+        key = "{}.{}".format(class_name, instance_id)
+        instance = all_instances.get(key, None)
 
 
-            if instance:
-                try:
-                    atttribute_value = eval(attribute_value)
-                except (NameError, SyntaxError):
-                    pass
+        if instance:
+            try:
+                atttribute_value = eval(attribute_value)
+            except (NameError, SyntaxError):
+                pass
 
-                setattr(instance, attribute_name, attribute_value)
-                instance.save()
-            else:
-                print("** no instance found **")
+            setattr(instance, attribute_name, attribute_value)
+            instance.save()
+        else:
+            print("** no instance found **")
             
 
     
